58aa912e10d6cd2aa378b377db6c2dd4e0fdfb01
portgozar <portgozarapp@gmail.com>
2023-05-25 01:22:28 +0330

    Init

---
+++ precache-manifest.1171e6e3c9b6bee66a8ccfdf06a91c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26da218fd7ed18122fa8742a3ea7dc1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79db4f70fad2ada032d0",
+    "url": "/static/css/main.51772d8f.chunk.css"
+  },
+  {
+    "revision": "7997c5f724a79a9783eb",
+    "url": "/static/js/2.b9809783.chunk.js"
+  },
+  {
+    "revision": "5031091b1f7f6986d4a47b9d37f83b0c",
+    "url": "/static/js/2.b9809783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79db4f70fad2ada032d0",
+    "url": "/static/js/main.e4e0087e.chunk.js"
+  },
+  {
+    "revision": "036cfb7bad84da798324",
+    "url": "/static/js/runtime-main.aab67829.js"
+  }
+]);
