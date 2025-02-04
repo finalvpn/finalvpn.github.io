0cddfa93815077dd0f8e6236e94d7ea9f0d67004
admin1 <admin1@admin1s-MacBook-Pro.local>
2025-02-04 09:02:13 +0000

    Init

---
+++ precache-manifest.411791489730ff5cca7fc4e5581a07bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0366f67593a398173c2ff89c5f81d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f32bc737bed59c3c6e2",
+    "url": "./static/css/main.1bf50a5f.chunk.css"
+  },
+  {
+    "revision": "28d92277fab5d5962d77",
+    "url": "./static/js/2.894f188e.chunk.js"
+  },
+  {
+    "revision": "0c29372aee40cd32979426f743d4c18b",
+    "url": "./static/js/2.894f188e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f32bc737bed59c3c6e2",
+    "url": "./static/js/main.1118bef8.chunk.js"
+  },
+  {
+    "revision": "84e5fcbfc07cc94d013d",
+    "url": "./static/js/runtime-main.948f7d3e.js"
+  },
+  {
+    "revision": "e7fd32eb34140495c37c5bbfdf14ea62",
+    "url": "./static/media/IRANSans(FaNum).e7fd32eb.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4fe83871db62c5f397cfa54ee7a526df.js"
+  "./precache-manifest.411791489730ff5cca7fc4e5581a07bc.js"
 );
 
 self.addEventListener('message', (event) => {
