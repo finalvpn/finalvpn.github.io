7df148d59977c80e06e438231259add0e3d2eeba
admin1 <admin1@admin1s-MacBook-Pro.local>
2025-02-06 14:39:13 +0000

    Init

---
+++ precache-manifest.38997b11bb72bb0dee665f5be5563b76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4878958cbc4d6d5ad5c1d5e44868a1e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc31539fc64a39b4367c",
+    "url": "./static/css/main.1bf50a5f.chunk.css"
+  },
+  {
+    "revision": "28d92277fab5d5962d77",
+    "url": "./static/js/2.894f188e.chunk.js"
+  },
+  {
+    "revision": "0c29372aee40cd32979426f743d4c18b",
+    "url": "./static/js/2.894f188e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc31539fc64a39b4367c",
+    "url": "./static/js/main.42c4a2a0.chunk.js"
+  },
+  {
+    "revision": "84e5fcbfc07cc94d013d",
+    "url": "./static/js/runtime-main.948f7d3e.js"
+  },
+  {
+    "revision": "e7fd32eb34140495c37c5bbfdf14ea62",
+    "url": "./static/media/IRANSans(FaNum).e7fd32eb.ttf"
+  }
+]);
